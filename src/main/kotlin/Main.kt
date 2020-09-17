07f5c20b7e491a2263632bdfa2dc26b2378731b2
trueddd <dmitry.kuzmich@orangesoft.co>
2020-09-17 21:10:17 +0300

    removed routing

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,12 @@
 import com.gitlab.kordlib.core.Kord
 import db.GuildsManager
 import io.ktor.application.Application
-import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.CORS
 import io.ktor.features.CallLogging
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
-import io.ktor.http.HttpStatusCode
 import io.ktor.request.path
-import io.ktor.response.respond
-import io.ktor.routing.get
-import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import kotlinx.coroutines.GlobalScope
@@ -21,7 +16,6 @@ import services.BaseBot.Companion.BOT_PREFIX
 import services.EpicGamesService
 import services.MainBot
 import services.SteamGamesService
-import services.TestBot
 import utils.AppEnvironment
 import utils.Commands
 import utils.provideDatabase
@@ -40,14 +34,8 @@ fun Application.module() {
     GlobalScope.launch {
         val client = Kord(AppEnvironment.getBotSecret())
 
-//        if (AppEnvironment.isProdEnv()) {
-            val bot = MainBot(guildsManager, epicGamesService, steamGamesService, client)
-            bot.attach()
-//        }
-//        if (AppEnvironment.isTestEnv()) {
-//            val bot = TestBot(guildsManager, epicGamesService, steamGamesService, client)
-//            bot.attach()
-//        }
+        val bot = MainBot(guildsManager, epicGamesService, steamGamesService, client)
+        bot.attach()
 
         client.login {
             listening("${BOT_PREFIX}${Commands.Common.HELP}")
@@ -67,12 +55,5 @@ fun Application.module() {
         header(HttpHeaders.Authorization)
         allowCredentials = true
         anyHost()
-    }
-
-    routing {
-
-        get("/") {
-            call.respond(HttpStatusCode.OK, "Server is up")
-        }
     }
 }
